d3c5b0e206fa166730aee2cf9fb02cc7c825f59f
preciousvictory <victoryexcel2018@gmail.com>
2023-04-29 17:00:55 -0700

    AirBnB clone v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,29 @@ Flask app
 """
 from api.v1.views import app_views
 from flask import Flask, render_template, jsonify
+from models import storage
+from models.state import State
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """status"""
     return (jsonify({"status": "OK"}))
+
+
+@app_views.route('/stats', methods=['GET'])
+def stas():
+    """ an endpoint that retrieves the number of each objects by type"""
+    dict_ = {}
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+    
+    for i in range(len(names)):
+        dict_[names[i]] = storage.count(classes[i])
+
+    return jsonify(dict_)
