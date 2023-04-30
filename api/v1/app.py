ff1e2cc968b10f38e171553e987ff6cf0a65253f
Osaclay <osaosun@gmail.com>
2023-04-30 06:12:28 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
-"""FLASK APP USING RESTful API"""
-from flask import Flask, render_template, jsonify
+"""Flask web application
+"""
+
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-import os
+from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ Remove the current SQLAlchemy Session """
+def teardown(error):
+    """Clean-up method
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    """ a handler for 404 errors """
-    return jsonify({"error": "Not found"}), 404
-
+def not_found(error):
+    """Custom 404 error
+    """
+    return jsonify({'error': 'Not found'}), 404
 
 if __name__ == '__main__':
-    host = os.getenv('HBNB_API_HOST')
-    port = os.getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=getenv('HBNB_API_HOST'),
+            port=getenv('HBNB_API_PORT'),
+            threaded=True)
